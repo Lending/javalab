3c16dd0fe7221295e2c8f40d54300dccece72a4c
Alpha <alpha@email.it>
2014-02-14 12:48:25 +0100

    Some changes

--- javalab-threading/src/flavio/giannini/javalab/threading/ChangeClass.java
+++ javalab-threading/src/flavio/giannini/javalab/threading/ChangeClass.java
@@ -2,4 +2,11 @@ package flavio.giannini.javalab.threading;
 
 public class ChangeClass {
 
+	/**
+	 * Adding things
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
 }
--- javalab-threading/src/flavio/giannini/javalab/threading/MainClass.java
+++ javalab-threading/src/flavio/giannini/javalab/threading/MainClass.java
@@ -1,5 +1,10 @@
 package flavio.giannini.javalab.threading;
 
+/**
+ * Some changes
+ * @author Giannini
+ *
+ */
 public class MainClass {
 
 }
